19789e0956423af59e79f8493126c958f10eaff3
SravantiNomula <47375870+SravantiNomula@users.noreply.github.com>
2020-08-26 22:15:25 +0530

    Update RoundRobinSimple.v

--- VERILOG/ARBITER/RR_SIMPLE/RoundRobinSimple.v
+++ VERILOG/ARBITER/RR_SIMPLE/RoundRobinSimple.v
@@ -28,31 +28,37 @@ module ArbSimpleRR (
   /////////////NO GRANT LOGIC////////////////////
   
   assign noGrant    = ~|grant[REQ_NUM-1:0];
-  //
+	
+ //////////////COUNTER LOGIC/////////////////////////////// 
   always @ (posedge clk, negedge rst_n) begin
     if (~rst_n)
 	   count[COUNTER_W-1:0] <= {COUNTER_W{1'b0}}; 
 	  else if (noGrant) begin
-	    if (count[COUNTER_W-1:0] == REQ_NUM-1)
-	      count[COUNTER_W-1:0] <= {COUNTER_W{1'b0}};
-	    else
-	      count[COUNTER_W-1:0] <= count[COUNTER_W-1:0] + 1'b1;
+//If we dont have a grant we keep counting through all the sources cyclically                                         	  
+	    	if (count[COUNTER_W-1:0] == REQ_NUM-1)		
+			count[COUNTER_W-1:0] <= {COUNTER_W{1'b0}};     //To wrap around
+	    	else
+		    count[COUNTER_W-1:0] <= count[COUNTER_W-1:0] + 1'b1 // WE increment the counter to point to next source every cycle if nogrant
 	  end
   end
-  //
-  //Create grant
+  ///////////////////////////////////////////////////
+  //CREATE GRANT LOGIC//////////////////////////////////// 
   generate
     genvar i;
     //
 	  for (i = 0; i < REQ_NUM; i=i+1) begin: uGrant
-      always @ (posedge clk, negedge rst_n) begin
+      always @ (posedge clk, negedge rst_n) 
+begin
         if (~rst_n)
 	      grant[i] <= 1'b0;
 	    else if (noGrant)
-	      grant[i] <= req[i] & (count[COUNTER_W-1:0] == i);
-      else
-        grant[i] <= req[i] & grant[i];
-      end
+		    grant[i] <= req[i] & (count[COUNTER_W-1:0] == i);// If we dont have any grant we keep on 
+	      					//Rotating from one source to another and give chance so that if it
+	      					//Raises a request we give it grant	
+     	    else
+		    grant[i] <= req[i] & grant[i];// If we have a grant already and again get a request
+						//then we keep the same count as shown above and keep the grant high
+ end
 	  end //for loop
   endgenerate
   //
